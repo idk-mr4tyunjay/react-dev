d88acf248b25f6ccd9199ab47dc18545f608e74f
Mruthunjay <idk.mratyunjay@gmail.com>
2024-06-02 17:05:30 +0530

    Project-status-done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import ProjectStatus from './components/Project/ProjectStatus'
 
 const App = () => {
   return (
     <>
     <div className='bg-custom'><Navbar /></div>
+    <ProjectStatus />
     </>
   )
 }
--- src/components/ProjectStatus.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProjectStatus = () => {
-  return (
-    <div>ProjectStatus</div>
-  )
-}
-
-export default ProjectStatus
